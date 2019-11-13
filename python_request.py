bb1744a8c70aeb17b4f7e03ae528f54282d20dba
NEHAISRANI <nehai18@navgurukul.org>
2019-11-13 09:54:46 +0530

    in this program i get the whole data from saral api and write courses.json file

---
+++ python_request.py
@@ -0,0 +1,19 @@
+import requests
+import json
+from pprint import pprint 
+
+#Javascript Object Notation
+print("********** WELCOME SARAL COURSES **********") 
+
+url="http://saral.navgurukul.org/api/courses"  #courses url
+def response(saral_url): 
+    response=requests.get(saral_url) 
+    data=response.json()
+    return (data) 
+dataResponse=response(url) 
+# pprint (dataResponse)
+ 
+def writeData(responseData):
+    with open("courses.json","w") as file1:
+        write_data=json.dump(responseData,file1,indent=2, sort_keys=True)
+writeData(dataResponse)
